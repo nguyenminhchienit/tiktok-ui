1359fc1c3f5fd10f854b7074571c332926d894a0
NguyenMinhChien <minhchien03112003@gmail.com>
2023-06-02 12:22:39 +0700

    done yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Following from './pages/Following';
+import { publicRoutes } from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Tiktok</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route) => {
+                        const Layout = route.component;
+                        return <Route path={route.path} element={<Layout></Layout>}></Route>;
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+import Following from './pages/Following';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
