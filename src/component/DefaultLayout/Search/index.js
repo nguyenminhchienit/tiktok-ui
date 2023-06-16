37fc4fd6334f141e8f209d173e49241952b74cd6
NguyenMinhChien <minhchien03112003@gmail.com>
2023-06-16 20:53:08 +0700

    Custom add useDebounce for search

--- src/component/DefaultLayout/Search/index.js
+++ src/component/DefaultLayout/Search/index.js
@@ -7,6 +7,7 @@ import Tippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '../Popper';
 import AccountItem from '../AccountItem';
 import { useEffect, useState, useRef } from 'react';
+import { useDebounce } from '../hooks';
 
 const cx = classNames.bind(styles);
 
@@ -16,16 +17,18 @@ function Search() {
     const [loading, setLoading] = useState(false);
     const [searchValue, setSearchValue] = useState('');
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
 
         setLoading(true);
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -34,7 +37,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
