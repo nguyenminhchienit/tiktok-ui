654f2512417f90b5a9487e5c88975fcd6a56b95d
NguyenMinhChien <minhchien03112003@gmail.com>
2023-06-17 23:29:27 +0700

    fix tippy more btn

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import Home from '../pages/Home';
 import Following from '../pages/Following';
 import Profile from '../pages/Profile';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/:nickname', component: Profile },
+    { path: '/@:nickname', component: Profile },
 ];
 
 const privateRoutes = [];
